8c7b08797b51a24b23a67e40cd186187a91edf83
Thanadet <89960060+Thanadett@users.noreply.github.com>
2024-12-18 16:03:46 +0700

    Update

--- User.java
+++ User.java
@@ -1,8 +1,8 @@
 import java.time.LocalDate;
 
 public class User {
-    private String name;
-    private LocalDate dob;
+    protected String name;
+    protected LocalDate dob;
 
     public User() {
         this.name = null;
