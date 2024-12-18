a9c29286757d3ac5725d7f956d090a7525fd2296
Thanadet <89960060+Thanadett@users.noreply.github.com>
2024-12-18 15:32:22 +0700

    commit

---
+++ Admin.java
@@ -0,0 +1,25 @@
+import java.time.LocalDate;
+
+public class Admin extends User {
+    public Admin() {
+        super();
+    }
+
+    public Admin (String name, int year, int month, int day) {
+        super(name, year, month, day);
+    }
+
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("User type: Admin.");
+    }
+
+    public void displayInfo(boolean full) {
+        if (full) {
+            displayInfo();
+            System.out.println("Date : " + LocalDate.now());
+        } else {
+            System.out.println("User Name : " + super.getName());
+        }
+    }
+}
---
+++ User.java
@@ -0,0 +1,29 @@
+import java.time.LocalDate;
+
+public class User {
+    private String name;
+    private LocalDate dob;
+
+    public User() {
+        this.name = null;
+        this.dob = LocalDate.now();
+    }
+
+    public User (String name, int year, int month, int day) {
+        this.name = name;
+        this.dob = LocalDate.of(year, month, day);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void displayInfo() {
+        System.out.println("User Name : " + name);
+        System.out.println("Birthday : " + dob);
+    }
+}
